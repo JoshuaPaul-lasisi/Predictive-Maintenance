b1046a92934a0bc485fbc3ad69b373aa401c9b1a
Paul-lasisi Joshua Oluwatobi <99060178+JoshuaPaul-lasisi@users.noreply.github.com>
2024-11-23 09:09:27 +0100

    Add files via upload

---
+++ project structure.sh
@@ -0,0 +1,36 @@
+PREDICTIVE-MAINTENANCE/
+├── data/
+│   ├── combined_data.csv
+│   ├── raw/
+│   │   ├── CE.txt
+│   │   ├── CP.txt
+│   │   ├── EPS1.txt
+│   │   ├── FS1.txt
+│   │   ├── FS2.txt
+│   │   ├── PS1.txt
+│   │   ├── PS2.txt
+│   │   ├── PS3.txt
+│   │   ├── PS4.txt
+│   │   ├── PS5.txt
+│   │   ├── PS6.txt
+│   │   ├── SE.txt
+│   │   ├── TS1.txt
+│   │   ├── TS2.txt
+│   │   ├── TS3.txt
+│   │   ├── TS4.txt
+│   │   ├── TS4.txt
+│   │   └── VS1.txt
+│   ├── description.txt
+│   ├── documentation.txt
+│   └── profile.txt
+├── models/
+├── notebooks/
+│   ├── data_loading.ipynb
+│   ├── data_preprocessing.ipynb
+│   └── eda.ipynb
+├── src/
+│   ├── dashboard.py
+│   └── data_loading.py
+├── project_structure.sh
+├── README.md
+└── Work notes.txt
